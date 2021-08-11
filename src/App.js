1731af34a2bbf9cda130099d473fc2d0b02f979d
Sumayya <sumayya.changaranchola@tarento.com>
2021-08-11 11:00:29 +0530

    table topics

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Topics from './components/Topics';
+
+
+function App() {
+  return (
+    <div>
+      <Topics/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Topics.js
@@ -0,0 +1,190 @@
+import React, { Component } from 'react';
+import './Topics.css';
+import { IconButton, Tooltip } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+class Topics extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            topics: [[
+                { word: "Tree", done: false, show:true },
+                { word: "Cat", done: false, show:false },
+                { word: "Umbrella", done: false, show:false },
+                { word: "Tomato", done: false, show:false },
+                { word: "Lap Top", done: false, show:false },
+                { word: "Forest", done: false, show:false },
+                { word: "Water", done: false, show:false },
+                { word: "Principal", done: false, show:false }
+            ], [
+                { word: "Bottle", done: false, show:true },
+                { word: "WiFi", done: false, show:false },
+                { word: "Squirrel", done: false, show:false },
+                { word: "Door", done: false, show:false },
+                { word: "Ship", done: false, show:false },
+                { word: "Grass", done: false, show:false },
+                { word: "Ball", done: false, show:false },
+                { word: "Fan", done: false, show:false }
+            ], [
+                { word: "Gum", done: false, show:true },
+                { word: "Cloud", done: false, show:false },
+                { word: "Iceland", done: false, show:false },
+                { word: "Rocket", done: false, show:false },
+                { word: "Elephant", done: false, show:false },
+                { word: "Ring", done: false, show:false },
+                { word: "Cup", done: false, show:false },
+                { word: "Hat", done: false, show:false }
+            ], [
+                { word: "America", done: false, show:true },
+                { word: "Monkey", done: false, show:false },
+                { word: "Scale", done: false, show:false },
+                { word: "Mobile", done: false, show:false },
+                { word: "River", done: false, show:false },
+                { word: "Mango", done: false, show:false },
+                { word: "Light", done: false, show:false },
+                { word: "Shirt", done: false, show:false }
+            ], [
+                { word: "Blue", done: false, show:true },
+                { word: "Mirror", done: false, show:false },
+                { word: "Bat", done: false, show:false },
+                { word: "Sun", done: false, show:false },
+                { word: "Bee", done: false, show:false },
+                { word: "Banana", done: false, show:false },
+                { word: "File", done: false, show:false },
+                { word: "Car", done: false, show:false }
+            ], [
+                { word: "Butterfly", done: false, show:true },
+                { word: "Snow", done: false, show:false },
+                { word: "Alarm", done: false, show:false },
+                { word: "Stick", done: false, show:false },
+                { word: "Village", done: false, show:false },
+                { word: "Fox", done: false, show:false },
+                { word: "Ant", done: false, show:false },
+                { word: "Moon", done: false, show:false }
+            ]],
+            page: 0,
+            tmShow: true,
+            toastmasters: [
+                "Sruthy",
+                "Sherin",
+                "Likhith",
+                "Sona",
+                "Kalyani",
+                "Shruthi"
+            ],
+            toastmaster: "",
+            input: ""
+        }
+    }
+    click = (i, j) => {
+        this.setState({
+            topics: this.state.topics.map((topics, index) => index !== i ?
+                topics : topics.map((topic, ix) => {
+                    if(ix === j) {
+                        return { ...topic, done: true }
+                    } else if(ix === j+1) {
+                        return { ...topic, show: true }
+                    } else{
+                        return topic
+                    }
+                }
+        ))});
+    }
+    paging = (a) => {
+        if (a) {
+            this.setState({ page: this.state.page - 1 });
+        } else {
+            this.setState({ page: this.state.page + 1, tmShow: true, toastmaster: "" })
+        }
+    }
+    random = () => {
+        let tm = this.state.toastmasters[Math.floor(Math.random() * this.state.toastmasters.length)];
+        this.select(tm);
+    }
+    select = (tm) => {
+        this.setState({ toastmaster: tm });
+        this.remove(tm);
+    }
+    remove = (tm) => {
+        this.setState({
+            toastmasters: this.state.toastmasters.filter(function (person) {
+                return person !== tm
+            })
+        });
+    }
+    add = (e) => {
+        e.preventDefault();
+        this.setState({
+            toastmasters: this.state.toastmasters.concat(this.state.input), input:""
+        })
+    }
+    changeHandler = (e) => {
+        this.setState({ input: e.target.value })
+    }
+    render() {
+        return (
+            <div style={{ margin: "50px" }}>
+                <div hidden={!this.state.tmShow}>
+                    <div style={{ width: '50%', marginLeft: '20%', marginTop: '2%' }}>
+                        <h1 style={{ color: "royalblue" }}>Toastmasters Table Topics <span hidden={this.state.toastmaster === ""}>
+                            <Tooltip aria-label="tt" placement="bottom-start" title={
+                                <React.Fragment><h2 color="inherit">Go to Table topic</h2></React.Fragment>}>
+                                <IconButton style={{ color: "royalblue", }} fontSize="large"><ArrowForwardIcon onClick={() => this.setState({ tmShow: false })} /></IconButton>
+                            </Tooltip></span></h1>
+                        <h2 style={{ color: "#d5212e" }}>Selected TM {this.state.toastmaster}</h2>
+                        <form onSubmit={this.add}>
+                            <input type='text' required onChange={this.changeHandler} value={this.state.input}
+                                style={{ width: "30%", height: "30px", marginBottom: "1%", marginTop: "1%" }} />
+                            <button
+                                type="submit"
+                                style={{ width: "15%", height: "30px", marginLeft: "5%", marginTop: '1%', backgroundColor: '#0f5778', color: 'white', cursor: "pointer" }}>
+                                ADD
+                            </button>
+                        </form>
+                        {this.state.toastmasters.map((tm, i) => (
+                            <h3 key={i}>
+                                <Tooltip aria-label="tt" placement="bottom-start" title={
+                                    <React.Fragment><h2 color="inherit">Select TM {tm}</h2></React.Fragment>}>
+                                    <span onClick={() => this.select(tm)} style={{ cursor: "pointer" }}>TM {tm} </span>
+                                </Tooltip>
+                                <IconButton style={{ color: "#d5212e" }}><DeleteIcon onClick={() => this.remove(tm)} /></IconButton></h3>
+                        ))}
+                        <button onClick={this.random}
+                            style={{ width: "15%", height: "40px", marginTop: '1%', backgroundColor: '#2fadcc', color: 'white', fontSize: "18px", cursor: "pointer" }}>
+                            RANDOM
+                        </button>
+                    </div>
+                </div>
+                {this.state.topics.map((topics, i) => (
+                    <div key={i} hidden={this.state.page !== i || this.state.tmShow}>
+                        <h3>TT: {this.state.page + 1}</h3>
+                        <button hidden={this.state.topics.length - 2 < this.state.page} style={{
+                            backgroundColor: "blue", color: "white", minWidth: "100px", fontSize: "20px", padding: "10px",
+                            marginRight: "100px", float: "right"
+                        }} onClick={() => this.paging(false)}>Next</button>
+                        <h4 style={{ cursor: "pointer" }} onClick={() => this.setState({ tmShow: true })}>
+                            <Tooltip aria-label="tt" placement="bottom-start" title={
+                                <React.Fragment><h2 color="inherit">Change TM {this.state.toastmaster}</h2></React.Fragment>}>
+                                <span>TM {this.state.toastmaster}</span></Tooltip></h4>
+                        {topics.map((topic, j) => (
+                            <div key={j} style={{ margin: "30px" }}>
+                                <button style={{
+                                    backgroundColor: "#61dafb", minWidth: "100px", fontSize: "20px", padding: "10px",
+                                    marginLeft: j * 200 + "px",
+                                }} disabled={topic.done} hidden={!topic.show}
+                                    onClick={() => this.click(i, j)}>{topic.word}</button>
+                                <br />
+                                <br />
+                            </div>
+                        ))}
+                        <button hidden={this.state.page === 0} onClick={() => this.paging(true)}>back</button>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+}
+
+export default Topics;
